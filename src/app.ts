f17b7d950887cd4b4b5fd9caa16364df23bdf6e7
Filip Gustavsson <filip.gustavsson@lindinvent.se>
2024-06-05 16:25:13 +0200

    TRY LEVELED!

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { SimpleTileViewer } from './simple-tile-viewer/simple-tile-viewer';
 import { LeveledTileViewer } from './leveled-tile-viewer/leveled-tile-viewer';
 
 export class App {
-  private _currentViewer = new SimpleTileViewer();
+  private _currentViewer = new LeveledTileViewer();
 
   constructor() {
     console.log('hello: ', this._currentViewer);
